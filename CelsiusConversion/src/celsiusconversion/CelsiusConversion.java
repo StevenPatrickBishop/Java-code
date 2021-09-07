52e19b266c0cba54457cad4d31a0d156407c189a
Steven Bishop <76878355+StevenPatrickBishop@users.noreply.github.com>
2021-09-07 00:14:41 -0400

    Add files via upload

---
+++ CelsiusConversion/src/celsiusconversion/CelsiusConversion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package celsiusconversion;
+
+/**
+ *
+ * @author SBISHOP
+ */
+
+
+/*Program will prompt user for a Celsuis temperature
+and will convert and output its fahrenheit equvilant.
+*/
+
+import java.util.Scanner;
+
+
+public class CelsiusConversion {
+
+ 
+    public static void main(String[] args) {
+     // create new Scanner object   
+     Scanner input = new Scanner(System.in);
+     
+        
+    }
+    
+}
